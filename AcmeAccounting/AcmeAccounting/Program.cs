4068e410fc216bd6c5f3cdbc17143f226e5ff987
kilgore80 <36181940+kilgore80@users.noreply.github.com>
2018-07-26 14:09:59 -0700

    Acme Accounting

---
+++ AcmeAccounting/AcmeAccounting/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Welcome to ACME Accounting Systems. Press Enter Please");
+        Console.ReadLine();
+        Console.WriteLine("Remember, we're " + " Accounting " + " on you!");
+        Console.Read();
+
+        }
+    }
+
