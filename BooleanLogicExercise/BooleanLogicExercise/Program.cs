831bbc28b1690fec1a972794f3b1d241e38367a3
kilgore80 <36181940+kilgore80@users.noreply.github.com>
2018-08-02 16:53:34 -0700

    bool logic

---
+++ BooleanLogicExercise/BooleanLogicExercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string appAge = Console.ReadLine();
+            int age = Convert.ToInt32(appAge);
+            Console.WriteLine("Have you ever had A DUI? true or false");
+            //bool ydui = true;
+            //bool ndui = false;
+            //string yesDui = Convert.ToString(ydui);
+            //string noDui = Convert.ToString(ndui);
+            bool dui = bool.Parse(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            string speedTix = Console.ReadLine();
+            int tix = Convert.ToInt32(speedTix);
+            bool qualified = (age > 15 && tix < 3 && dui == false);
+            Console.Write("Are you qualified? " + qualified);
+            Console.ReadLine();
+             
+        }
+    }
+}
---
+++ BranchingExercise/BranchingExercise/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("What is the weight of your package?");
+            string lb = Console.ReadLine();
+            int packWeight = Convert.ToInt32(lb);
+            if (packWeight < 50)
+            {
+                Console.WriteLine("Please enter your package width:");
+                string width = Console.ReadLine();
+                int wid = Convert.ToInt32(width);
+                Console.WriteLine("Please enter your package height:");
+                string height = Console.ReadLine();
+                int hei = Convert.ToInt32(height);
+                Console.WriteLine("Please enter your package length:");
+                string length = Console.ReadLine();
+                int len = Convert.ToInt32(length);
+                int sum = (wid + hei + len);
+            };
+            if (50 > (sum))
+            {
+                Console.WriteLine("")
+            }
+
+            
+
+
+            else
+            {
+                Console.WriteLine("Package is too big to be shipped via Package Express. Have a good day");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
