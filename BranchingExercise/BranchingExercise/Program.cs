3ca480d46b5cea92155382ea018473180d4dca47
kilgore80 <36181940+kilgore80@users.noreply.github.com>
2018-08-03 10:42:00 -0700

    Branching exercise

--- BranchingExercise/BranchingExercise/Program.cs
+++ BranchingExercise/BranchingExercise/Program.cs
@@ -16,7 +16,11 @@ namespace BranchingExercise
             Console.WriteLine("What is the weight of your package?");
             string lb = Console.ReadLine();
             int packWeight = Convert.ToInt32(lb);
-            if (packWeight < 50)
+            if (packWeight > 50)
+            {
+                Console.WriteLine("Package is too big to be shipped via Package Express. Have a good day");
+            }
+            else if (packWeight < 50)
             {
                 Console.WriteLine("Please enter your package width:");
                 string width = Console.ReadLine();
@@ -27,23 +31,16 @@ namespace BranchingExercise
                 Console.WriteLine("Please enter your package length:");
                 string length = Console.ReadLine();
                 int len = Convert.ToInt32(length);
-                int sum = (wid + hei + len);
-            };
-            if (50 > (sum))
-            {
-                Console.WriteLine("")
+                int sum1 = (wid * hei * len);
+                int sum2 = sum1 * packWeight;
+                int sum3 = sum2 / 100;
+             if (sum1 > 50)
+                    Console.WriteLine("Sorry to inform you that the package you are trying to ship is too big to be shipped via Package Express. Thank you.");
+                else
+                    Console.WriteLine("Your estimated total for shipping this package is: " + "$" + sum3 + " . " + " Thank you for your business. ");
             }
 
-            
-
-
-            else
-            {
-                Console.WriteLine("Package is too big to be shipped via Package Express. Have a good day");
-            }
-
-
-            Console.ReadLine();
+                Console.ReadLine();
         }
     }
 }
