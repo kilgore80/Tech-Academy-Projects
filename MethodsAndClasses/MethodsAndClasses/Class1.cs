7102d4dae2bec5a09e46da661dbeff2701eae0c0
kilgore80 <36181940+kilgore80@users.noreply.github.com>
2018-08-09 16:28:20 -0700

    Method exercise

---
+++ MethodsAndClasses/MethodsAndClasses/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndClasses
+{
+    public class Class1
+    {
+        public Class1()
+        {
+             
+        }
+
+        public int Num1(int input1)
+        {
+          
+
+            int results = input1 + 5;
+            return results;
+        }
+        public int Num2(int input2)
+        {
+            int results2 = input2 * 8;
+            return results2;
+        }
+        public int Num3(int input3)
+        {
+            int results3 = input3 - 2;
+            return results3;
+        }
+            
+    }
+}
---
+++ MethodsAndClasses/MethodsAndClasses/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 NumMath = new Class1();
+
+           
+            Console.WriteLine("What number would you like to do math operations on?");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int results = NumMath.Num1(userInput);
+            int results2 = NumMath.Num2(userInput);
+            int results3 = NumMath.Num3(userInput);
+            Console.WriteLine(userInput + " Added to five is: " + results);
+            Console.WriteLine(userInput + " Multiplied by eight is: " + results2);
+            Console.WriteLine(userInput + " Subtracted by two: " + results3);
+            //int userInput2 = Convert.ToInt32(Console.ReadLine());
+            //int results2 = NumMath.Num2(userInput2);
+           // Console.WriteLine(results2);
+            Console.ReadLine();
+        }
+
+       
+
+    }
+}
