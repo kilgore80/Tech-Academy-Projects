91d1725a4e6a533909d133f4a1db1ad35ce19a7e
kilgore80 <36181940+kilgore80@users.noreply.github.com>
2018-08-10 16:35:15 -0700

    Methods Exercise 2

---
+++ MethodsExercise/MethodsExercise/ClassNum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise
+{
+   public class ClassNum
+    {
+        public ClassNum()
+        {
+
+        }
+        public int NumMethod(int Num1)
+        {
+            int results = Num1 * 13;
+            return results;
+        }
+        public float NumMethod(float Num2)
+        {
+            float results2 = Num2 + 8.37f;
+            int Convo = Convert.ToInt32(results2);
+            return Convo;
+        }
+        public int NumMethod(string goat)
+        {
+            int cat = Convert.ToInt32(goat);
+            int results3 = cat + 20;
+            return results3;
+        }
+    }
+}
---
+++ MethodsExercise/MethodsExercise/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            ////Method One
+
+            ClassNum Dog = new ClassNum();
+            //int input1 = 8;
+            //int Results = Dog.NumMethod(input1);
+            //Console.WriteLine(Results);
+            //Console.ReadLine();
+
+            //Method Two
+
+            float input2 = 4.25f;
+            float Results2 = Dog.NumMethod(input2);
+            Console.WriteLine(Results2);
+            Console.ReadLine();
+
+            //Method Three
+
+            string input3 = "20";
+            //int ren = Convert.ToInt32(input3);
+            int Results3 = Dog.NumMethod(input3);
+            Console.WriteLine(Results3);
+            Console.ReadLine();
+
+        }
+    }
+}
