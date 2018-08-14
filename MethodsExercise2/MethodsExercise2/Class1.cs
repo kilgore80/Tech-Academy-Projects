2a45ba425556c6c3ca4cfaf526ab7b49329e1568
kilgore80 <36181940+kilgore80@users.noreply.github.com>
2018-08-14 10:19:01 -0700

    Method exercise 2

---
+++ MethodsExercise2/MethodsExercise2/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise2
+{
+    public class Class1
+    {
+        public Class1()
+        {
+
+        }
+        public int NumMethod(int Num1, int Num2 = 2)
+        {
+            int results = Num1 * Num2;
+            return results;
+        }
+    }
+}
---
+++ MethodsExercise2/MethodsExercise2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 Cat = new Class1();
+            Console.WriteLine("Please write a number.");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("If you'd like, enter another number.");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+
+            //int input1 = 10;
+            int Results = Cat.NumMethod(input1, input2);
+            Console.WriteLine(Results);
+            Console.ReadLine();
+        }
+    }
+}
