d98059146e61bec1e8813f28352b74f355821f90
kilgore80 <36181940+kilgore80@users.noreply.github.com>
2018-08-14 16:52:44 -0700

    again

--- ClassMethods/ClassMethods/Class1.cs
+++ ClassMethods/ClassMethods/Class1.cs
@@ -16,10 +16,30 @@ namespace ClassMethods
         public void NumMeth(int Num1)
         {
             int results = Num1 / 2;
-           // Console.WriteLine(Num1 + " divided by two = " + results);
-           // Console.ReadLine();
+            Console.WriteLine(Num1 + " divided by two = " + results);
+            Console.ReadLine();
         }
-        public int NumMeth2()
-       
+        public void NumMeth2(out int i, out int x)
+        {
+            i = 10;
+            x = 5;
+            i *= i;
+            x *= x;
+        }
+        public int NumMeth3(int Wal, int Gal )
+        {
+            int results2 = Wal + Gal;
+            return results2;
+        }
+        public float NumMeth3(float Tree, float Day)
+        {
+            float results3 = Tree + Day;
+            return results3;
+        }
+        public string NumMeth3(string rat, string kid)
+        {
+            string results4 = rat + kid;
+            return results4;
+        }
     }
 }
--- ClassMethods/ClassMethods/Program.cs
+++ ClassMethods/ClassMethods/Program.cs
@@ -9,12 +9,33 @@ namespace ClassMethods
     class Program
     {
         static void Main(string[] args)
+        //{
+        //Class1 goat = new Class1();
+        //    Console.WriteLine("Please enter a number.");
+        //    int input1 = Convert.ToInt32(Console.ReadLine());
+
+        //    goat.NumMeth(input1);
+        //}
+
+        //4. Create a method =  with output parameters.
+
+        //{
+        //  Class1 goat = new Class1();
+
+        //  int a, b;
+        //  goat.NumMeth2(out a, out b);
+        //  Console.WriteLine("a Value: {0}", a);
+        //  Console.WriteLine("b Value: {0}", b);
+        //  Console.ReadLine();
+        //}
+
+        //5. Overload a method.
         {
             Class1 goat = new Class1();
-            Console.WriteLine("Please enter a number.");
-            int input1 = Convert.ToInt32(Console.ReadLine());
 
-            goat.NumMeth(input1);
+            int input1 = 
         }
+
+
     }
 }
