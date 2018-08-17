3964dc227c3104e3e4f17dd97f80967e27020540
kilgore80 <36181940+kilgore80@users.noreply.github.com>
2018-08-17 10:56:09 -0700

    overloading ops

---
+++ OverloadingOperatorExercise/OverloadingOperatorExercise/Employee.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingOperatorExercise
+{
+    public class Employee
+    {
+        public string Firstname { get; set; }
+        public int Id { get; set; }
+        public string Lastname { get; set; }
+
+        public static bool operator== (Employee employee1, Employee employee2)
+        {
+            return Equals(employee1.Id, employee2.Id);
+            //bool results;
+            //if(employee1.Id == employee2.Id)
+            //{
+            //    results = true;
+            //    return results;
+            //}
+
+            //else
+            //{
+            //    results = false;
+            //    return results;
+            //}
+
+        }
+
+        public static bool operator!= (Employee employee1, Employee employee2)
+        {
+            return !Equals(employee1.Id, employee2.Id);
+            //bool results2;
+            //if (employee1.Id != employee2.Id)
+            //{
+            //    results2 = true;
+            //    return results2;
+            //}
+            //else
+            //{
+            //    results2 = false;
+            //    return results2;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+    }
+
+   
+
+
+    }
+
+
---
+++ OverloadingOperatorExercise/OverloadingOperatorExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingOperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            //int emp1 = 45256;
+            //int emp2 = 58536;
+
+            Employee Dan = new Employee { Firstname = "Dan", Lastname = "Harrmon", Id = 1};
+            Employee Jill = new Employee { Firstname = "Jill", Lastname = "Smith", Id = 2};
+            bool results = (Dan == Jill);
+
+            //bool results = employee.Equals(Dan == emp2);
+            Console.WriteLine(results);
+            Console.ReadLine();
+        }
+    }
+}
