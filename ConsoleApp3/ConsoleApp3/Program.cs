23c836f0bb70c6e9c62220f4b1fc39d98db45552
kilgore80 <36181940+kilgore80@users.noreply.github.com>
2018-08-18 09:05:50 -0700

    Generic Classes

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -15,21 +15,22 @@ namespace ConsoleApp3
             employee.Things.Add("John");
             employee.Things.Add("Sally");
             employee.Things.Add("Dan");
-
-            
-
-
+            for (int y = 0; y < employee.Things.Count; y++)
+            {
+                Console.WriteLine(employee.Things[y]);
+            }
             Employee<int> age = new Employee<int>();
             age.Things = new List<int>();
             age.Things.Add(45);
             age.Things.Add(32);
             age.Things.Add(25);
 
-            for (int i = 0; i < 3; i++ )
-
-            Console.WriteLine(i);
+            for (int i = 0; i < age.Things.Count; i++)
+            {
+                Console.WriteLine(age.Things[i]);
+              
+            }
             Console.ReadLine();
-
 
         }
     }
