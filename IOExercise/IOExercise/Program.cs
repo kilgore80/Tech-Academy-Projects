530e1ddfe6ac49b6e711833e352b8ee04dbf447d
kilgore80 <36181940+kilgore80@users.noreply.github.com>
2018-08-28 10:29:36 -0700

    IO Exercise

---
+++ IOExercise/IOExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IOExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Would you please enter a number?");
+            string answer = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\kilgo\OneDrive\Documents\GitHub\Tech-Academy-Projects\IOExercise\number\number.txt", answer);
+            string answer2 = File.ReadAllText(@"C:\Users\kilgo\OneDrive\Documents\GitHub\Tech-Academy-Projects\IOExercise\number\number.txt");
+            Console.WriteLine("This was the number you picked: " + answer2);
+            Console.ReadLine();
+        }
+    }
+}
